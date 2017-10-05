e6da1e9d8123a09f1f03f02f3ad1bfe9c299e696
Jared Mashburn <jmashbur@redhat.com>
2017-10-05 15:48:37 -0400

    Adding Update

--- index.php
+++ index.php
@@ -7,13 +7,14 @@ use Aws\Ec2\Ec2Client;
 
 class HelloHandler {
     function get() {
-  		$ec2Client = new Ec2Client([
-    		'region' => 'us-west-2',
-    		'version' => '2016-11-15',
-    		'profile' => 'default'
-		]);
-     	$result = $ec2Client->describeInstances();
-		var_dump($result); 
+        echo sprintf("AWS_ACCESS_KEY_ID: %s, AWS_SECRET_ACCESS_KEY: %s", getenv("AWS_ACCESS_KEY_ID"), getenv("AWS_SECRET_ACCESS_KEY"));
+  		#$ec2Client = new Ec2Client([
+    	#	'region' => 'us-west-2',
+    	#	'version' => '2016-11-15',
+    	#	'profile' => 'default'
+		#]);
+     	#$result = $ec2Client->describeInstances();
+		#var_dump($result); 
     }
 }
 
