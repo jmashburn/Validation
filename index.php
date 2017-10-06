e220732b74b019c7c68ba6b36ef06d1960f8f42f
Jared Mashburn <jmashbur@redhat.com>
2017-10-06 10:07:46 -0400

    Testing sdk

--- index.php
+++ index.php
@@ -7,14 +7,13 @@ use Aws\Ec2\Ec2Client;
 
 class HelloHandler {
     function get() {
-        echo sprintf("AWS_ACCESS_KEY_ID: %s, AWS_SECRET_ACCESS_KEY: %s", getenv("AWS_ACCESS_KEY_ID"), getenv("AWS_SECRET_ACCESS_KEY"));
-  		#$ec2Client = new Ec2Client([
-    	#	'region' => 'us-west-2',
-    	#	'version' => '2016-11-15',
-    	#	'profile' => 'default'
-		#]);
-     	#$result = $ec2Client->describeInstances();
-		#var_dump($result); 
+  		$ec2Client = new Ec2Client([
+    		'region' => 'us-west-2',
+    		'version' => '2016-11-15',
+    		'profile' => 'default'
+		]);
+     	$result = $ec2Client->describeInstances();
+		var_dump($result); 
     }
 }
 
